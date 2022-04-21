c7efec780a9e11e97690a19cf751533279788e79
jwijenbergh <jeroenwijenbergh@protonmail.com>
2022-04-21 14:47:42 +0200

    Python: Remove obsolete FIXMEs

--- wrappers/python/eduvpncommon/main.py
+++ wrappers/python/eduvpncommon/main.py
@@ -10,8 +10,7 @@ class StateType(Enum):
 
 # Registers the python app with the Go code
 # name: The name of the app to be registered
-# url: The url of the server to connect to, FIXME: To be removed
-# state_callback: The callback to trigger whenever a state is changed, FIXME: Remove whenever this wrapper has implemented callbacks using function decorations
+# state_callback: The callback to trigger whenever a state is changed
 def Register(name, config_directory, state_callback, debug):
     name_bytes = name.encode("utf-8")
     dir_bytes = config_directory.encode("utf-8")
