fe5f8c80298e46a6940e3ad087f559942edb4a06
jwijenbergh <jeroenwijenbergh@protonmail.com>
2024-02-07 13:45:10 +0100

    API + Server: Support vpn_proto_transport_list

--- internal/api/profiles/profiles.go
+++ internal/api/profiles/profiles.go
@@ -9,6 +9,7 @@ type Profile struct {
 	ID               string   `json:"profile_id"`
 	DisplayName      string   `json:"display_name"`
 	VPNProtoList     []string `json:"vpn_proto_list"`
+	VPNProtoTransportList []string `json:"vpn_proto_transport_list"`
 	DefaultGateway   bool     `json:"default_gateway"`
 	DNSSearchDomains []string `json:"dns_search_domain_list"`
 }
@@ -41,6 +42,23 @@ func (i Info) MustIndex(n int) Profile {
 func hasProtocol(protos []string, proto protocol.Protocol) bool {
 	for _, p := range protos {
 		if protocol.New(p) == proto {
+			return true
+		}
+	}
+	return false
+}
+
+func (p *Profile) ShouldFailover() bool {
+	// old servers don't support it, only failover in case OpenVPN is supported
+	if len(p.VPNProtoTransportList) == 0 {
+		// this checks VPNProtoList
+		return p.HasOpenVPN()
+	}
+	for _, c := range p.VPNProtoTransportList {
+		if c == "wireguard+tcp" {
+			return true
+		}
+		if c == "openvpn+tcp" {
 			return true
 		}
 	}
