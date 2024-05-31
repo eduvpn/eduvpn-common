9f9c74cb55f3a0be1799669dca08546747cbf3c2
jwijenbergh <jeroenwijenbergh@protonmail.com>
2024-05-31 13:52:26 +0200

    i18nerr: update cause sprintf

--- i18nerr/i18nerr.go
+++ i18nerr/i18nerr.go
@@ -52,7 +52,7 @@ func (e *Error) translated(t language.Tag) string {
 	})
 	msg := printerOrNew(t).Sprintf(e.key, e.args...)
 	if e.wrapped != nil {
-		return msg + ". " + printerOrNew(t).Sprintf("The cause of the error is:") + " " + e.wrapped.Error()
+		return printerOrNew(t).Sprintf("%s. The cause of the error is: %s", msg, e.wrapped.Error())
 	}
 	return msg
 }
