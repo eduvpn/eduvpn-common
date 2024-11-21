72edbfe5d2d51b7dbaaa6ac6fe96cfbd43b60069
jwijenbergh <jeroenwijenbergh@protonmail.com>
2024-11-21 15:52:07 +0100

    Remove old Docker CI

--- ci/docker/healthcheck.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Check if wg show has any output
-output_wg="$(wg show)"
-[[ -n $output_wg ]]
--- ci/docker/replaceexpiry.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# If no custom expiry set, do nothing
-[ -z "${OAUTH_EXPIRED_TTL}" ] && exit
-
-# Replace oauth expiry
-sed -i "s/return \[/return \[\n'Api' => [\n'tokenExpiry' => 'PT${OAUTH_EXPIRED_TTL}S',\n],/g" "$1"
